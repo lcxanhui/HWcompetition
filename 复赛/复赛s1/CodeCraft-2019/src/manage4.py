2821e3196b4ccb90b0c7e2aaed87c65c8ff904de
杨超越求offer <37524798+lcxanhui@users.noreply.github.com>
2019-05-17 22:38:18 +0800

    复赛训练赛代码

---
+++ 复赛/复赛s1/CodeCraft-2019/src/manage4.py
@@ -0,0 +1,171 @@
+from shortest_path import *
+
+
+class Manage(object):
+	def __init__(self, data_car, data_road, data_cross, data_preset, preset_time):
+		self.data_car = data_car
+		self.data_road = data_road
+		self.data_cross = data_cross
+		self.data_preset = data_preset
+
+		self.use_roadNum = dict()
+		self.crossId2roadId = dict()
+		self.crossIds = ''
+		self.route = dict()  # 开始结束路口到最小路径映射
+		self.path, self.id2index = floyd(self.data_road, self.data_cross, self.crossIds)
+
+		self.road_dic = dict()
+		for road in self.data_road:
+			start2end = road.startId + '_' + road.endId
+			self.crossId2roadId[start2end] = road
+			self.road_dic[road.roadId] = start2end
+			if road.isDuplex == '1':
+				cross_ids = road.endId + '_' + road.startId
+				self.crossId2roadId[cross_ids] = road
+				# self.road_dic[road.roadId] = cross_ids
+
+
+			# 车辆按速度排序
+		sort_carByv = list()
+		car_dictionary = dict()
+
+		for car in self.data_car:
+			if car.preset == 1:
+				car.planTime = preset_time[car.carId]
+			if car.startId in car_dictionary.keys():
+				car_dictionary[car.startId].append(car)
+			else:
+				car_dictionary[car.startId] = [car]
+
+		key_car = list(car_dictionary.keys())
+		max_length = 0
+		for key in key_car:
+			car_dictionary[key].sort(key=lambda x:(-x.priority, x.planTime, -x.maxSpeed))
+			car_num = len(car_dictionary[key])
+			if car_num > max_length:
+				max_length = car_num
+
+		for i in range(max_length):
+			for key in key_car:
+				if len(car_dictionary[key]) > i:
+					sort_carByv.append(car_dictionary[key][i])
+		self.data_car = sort_carByv
+
+
+	def running(self):
+		results = ''
+		num = 0
+		block_road = 4500
+		everyTime = 14
+		# for i in range(10):
+		# 	for car in self.data_car[num//10*i : num//10*(i+1)]:
+		# 		# if car.preset == 1:
+		# 		# 	results += self.data_preset[car.carId]
+		# 		# 	continue
+		# 		cross_idStart = car.startId
+		# 		cross_idEnd = car.endId
+		# 		if car.preset == 1:
+		# 			# if(car.carId == '31330'):
+		# 			# 	print('oo')
+		# 			min_road = self.data_preset[car.carId]
+		# 			min_path = list()
+		# 			last = ''
+		# 			for road in min_road:
+		# 				s2e = self.road_dic[road]
+		# 				data = s2e.split('_')
+		# 				# last = ''
+		# 				if data[0] == car.startId:
+		# 					min_path.append(data[0])
+		# 					min_path.append(data[1])
+		# 					last = data[1]
+		# 				elif data[1] == car.startId:
+		# 					min_path.append(data[1])
+		# 					min_path.append(data[0])
+		# 					last = data[0]
+		# 				elif last == data[0]:
+		# 					min_path.append(data[1])
+		# 					last = data[1]
+		# 				else:
+		# 					min_path.append(data[0])
+		# 					last = data[0]
+		# 		else:
+		# 			min_path = shortest_path(self.data_cross, self.path, self.id2index[cross_idStart], self.id2index[cross_idEnd])
+		# 		road_data = list()
+		# 		# print(car.carId)
+		# 		for j in range(len(min_path) - 1):
+		# 			self.crossIds = min_path[j] + '_' + min_path[j + 1]
+		# 			if self.crossIds in self.use_roadNum.keys():
+		# 				self.use_roadNum[self.crossIds] += 1
+		# 			else:
+		# 				self.use_roadNum[self.crossIds] = 0
+		# 			if self.use_roadNum[self.crossIds] > block_road:
+		# 				self.path, self.id2index = floyd(self.data_road, self.data_cross, self.crossIds)
+		# 				self.use_roadNum[self.crossIds] = 0
+		# 			road_data.append(self.crossId2roadId[self.crossIds])
+		# 		road_ids = ''
+		# 		for k in range(len(road_data) - 1):
+		# 			road_ids += road_data[k].roadId + ','
+		# 		road_ids += road_data[-1].roadId
+		# 		if car.preset == 1:
+		# 			result = str('(' + car.carId + ',' + str(car.planTime) + ',' + road_ids + ')')
+		# 		else:
+		# 			result = str('(' + car.carId + ',' + str(car.planTime + i) + ',' + road_ids + ')')
+		# 		results += str(result) + '\n'
+
+		long = (len(self.data_car) - num) // everyTime
+		for i in range(long):
+			if i + 1 == long:
+				movingCars = self.data_car[everyTime * i + num:]
+			else:
+				movingCars = self.data_car[everyTime * i + num: everyTime * (i + 1) + num]
+			for car in movingCars:
+				cross_idStart = car.startId
+				cross_idEnd = car.endId
+				if car.preset == 1:
+					min_road = self.data_preset[car.carId]
+					min_path = list()
+					last = ''
+					for road in min_road:
+						s2e = self.road_dic[road]
+						data = s2e.split('_')
+						# last = ''
+						if data[0] == car.startId:
+							min_path.append(data[0])
+							min_path.append(data[1])
+							last = data[1]
+						elif data[1] == car.startId:
+							min_path.append(data[1])
+							min_path.append(data[0])
+							last = data[0]
+						elif last == data[0]:
+							min_path.append(data[1])
+							last = data[1]
+						else:
+							min_path.append(data[0])
+							last = data[0]
+				else:
+					min_path = shortest_path(self.data_cross, self.path, self.id2index[cross_idStart], self.id2index[cross_idEnd])
+				road_data = list()
+				for j in range(len(min_path) - 1):
+					self.crossIds = min_path[j] + '_' + min_path[j + 1]
+					if self.crossIds in self.use_roadNum.keys():
+						self.use_roadNum[self.crossIds] += 1
+					else:
+						self.use_roadNum[self.crossIds] = 0
+					if self.use_roadNum[self.crossIds] > block_road:
+						self.path, self.id2index = floyd(self.data_road, self.data_cross, self.crossIds)
+						self.use_roadNum[self.crossIds] = 0
+					road_data.append(self.crossId2roadId[self.crossIds])
+				road_ids = ''
+				for k in range(len(road_data) - 1):
+					road_ids += road_data[k].roadId + ','
+				road_ids += road_data[-1].roadId
+				if car.preset == 1:
+					result = str('(' + car.carId + ',' + str(car.planTime) + ',' + road_ids + ')')
+				else:
+					result = str('(' + car.carId + ',' + str(car.planTime + i) + ',' + road_ids + ')')
+				results += str(result) + '\n'
+		return results
+
+
+
